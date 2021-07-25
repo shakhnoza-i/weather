6cc4e0204ed92c3c7e7acca3675f94d9bd48cdbd
shakhnoza-i <84865440+shakhnoza-i@users.noreply.github.com>
2021-07-25 12:21:29 +0600

    Update views.py

--- weather/views.py
+++ weather/views.py
@@ -1,32 +1,23 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework import generics
-from rest_framework.generics import GenericAPIView
-from rest_framework.response import Response 
+from rest_framework.generics import ListAPIView
 from .models import Weather
 from .serializers import WeatherSerializer
 import requests
  
-def index(request):
-    city = 'canberra'
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=c99159f47dfcfa9dce5fbc667a06d2e2'
-    city_weather = requests.get(url).json() 
-    print(city_weather)
-    weather = {
-        'city' : city,
-        'temperature' : city_weather['main']['temp'],
-        'description' : city_weather['weather'][0]['description'],
-    }    
-    return JsonResponse(weather)
-    #return render(request, 'weather/index.html', context) 
-    
-
-class WeatherDetail(generics.RetrieveAPIView):
+class WeatherDetail(ListAPIView):
     queryset = Weather.objects.all()
     serializer_class = WeatherSerializer
 
-
-    def create(self, request):
-        #weather = self.get_object(index)
-        return index
-
+    def list(self, request):
+        city = 'canberra'
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=c99159f47dfcfa9dce5fbc667a06d2e2'
+        city_weather = requests.get(url).json() 
+        print(city_weather)
+        weather = {
+            'city' : city,
+            'temperature' : city_weather['main']['temp'],
+            'description' : city_weather['weather'][0]['description'],
+        }    
+        return JsonResponse(weather)
