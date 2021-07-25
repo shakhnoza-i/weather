d762a43f0992902d37291d67388b908d441c7730
shakhnoza-i <84865440+shakhnoza-i@users.noreply.github.com>
2021-07-25 12:22:20 +0600

    Update urls.py

--- weather/urls.py
+++ weather/urls.py
@@ -1,13 +1,7 @@
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 from .views import WeatherDetail
 
-
-
 urlpatterns = [
-    path('', views.index),  
-    path('weather/<int:pk>/', WeatherDetail.as_view(), name ='weather'),
+    path('weather/', WeatherDetail.as_view(), name ='weather'),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
